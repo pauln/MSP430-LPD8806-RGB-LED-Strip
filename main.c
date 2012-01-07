25fbf8376a5f021241853f2dd53d30b31e3a9a73
Paul Nicholls <paul.n@next.gen.nz>
2012-01-07 13:23:06 +1300

    Coding style changes (two-space indent, no newline before {)

--- main.c
+++ main.c
@@ -23,13 +23,13 @@
 #include <legacymsp430.h>
 
 // constant defines
-#define DATA BIT7
-#define CLOCK BIT6
+#define DATA    BIT7
+#define CLOCK   BIT6
 #define NUMLEDS 64
 
 // wdt delay constants
 #define MCLK_FREQUENCY      1000000
-#define WDT_DIVIDER        512
+#define WDT_DIVIDER         512
 
 const unsigned long WDT_FREQUENCY = MCLK_FREQUENCY / WDT_DIVIDER;
 volatile unsigned long wdtCounter = 0;
@@ -74,318 +74,273 @@ unsigned long white = 0x808080 | 0x80FFFF;
 
 unsigned long randomcolor;
 
-void main(void)
-{
-	//WDTCTL = WDTPW + WDTHOLD;
-	
-	// use 1MHz calibrated values
-	DCOCTL = CALDCO_1MHZ;
-	BCSCTL1 = CALBC1_1MHZ;
-	// wdt set as interval
-	WDTCTL = WDTPW + WDTTMSEL + WDTIS1;
-	// wdt interrupt
-	IE1 |= WDTIE;
-	// enable global interrupts using intrinsic
-	__enable_interrupt();
-	
-	// initialize pins for SPI
-	init();
-	
-	colorwipe(clear); // clear led strip
-	delayMillis(1000);
-	while (1)
-	{	
-		demos();
-	}
+void main(void) {
+  //WDTCTL = WDTPW + WDTHOLD;
+  
+  // use 1MHz calibrated values
+  DCOCTL = CALDCO_1MHZ;
+  BCSCTL1 = CALBC1_1MHZ;
+  // wdt set as interval
+  WDTCTL = WDTPW + WDTTMSEL + WDTIS1;
+  // wdt interrupt
+  IE1 |= WDTIE;
+  // enable global interrupts using intrinsic
+  __enable_interrupt();
+  
+  // initialize pins for SPI
+  init();
+  
+  colorwipe(clear); // clear led strip
+  delayMillis(1000);
+  while (1) { 
+    demos();
+  }
 
 }
 
 /* use functions */
 
 // random chase
-void randomchase(void)
-{
-	int m;
-	for ( m = 0; m <= NUMLEDS; m++ )
-	{
-		pixels[m] = adcGenRand24();
-		pixels[m-1] = clear;
-		display();
-		delayMillis(100);
-	}
-	for ( m = NUMLEDS; m >= 0; m-- )
-	{
-		pixels[m] = adcGenRand24();
-		pixels[m+1] = clear;
-		display();
-		delayMillis(100);
-	}
+void randomchase(void) {
+  int m;
+  for ( m = 0; m <= NUMLEDS; m++ ) {
+    pixels[m] = adcGenRand24();
+    pixels[m-1] = clear;
+    display();
+    delayMillis(100);
+  }
+  for ( m = NUMLEDS; m >= 0; m-- ) {
+    pixels[m] = adcGenRand24();
+    pixels[m+1] = clear;
+    display();
+    delayMillis(100);
+  }
 }
 
 // police light bar chaser
-void copcar(void)
-{
-	int m;
-	int middle = NUMLEDS / 2 ;
+void copcar(void) {
+  int m;
+  int middle = NUMLEDS / 2 ;
 
-	colorwipe(clear);
+  colorwipe(clear);
 
-	for ( m = 0; m < NUMLEDS; m++ )
-	{
-		if ( m <= middle )
-		{
-			pixels[m] = red;
-			pixels[m - 2] = clear;
+  for ( m = 0; m < NUMLEDS; m++ ) {
+    if ( m <= middle ) {
+      pixels[m] = red;
+      pixels[m - 2] = clear;
 
-			pixels[NUMLEDS - m] = blue;
-			pixels[NUMLEDS - m + 2] = clear;
-		}
-		display();
+      pixels[NUMLEDS - m] = blue;
+      pixels[NUMLEDS - m + 2] = clear;
+    }
+    display();
 
-		if  ( m >= middle )
-		{
-			pixels[m] = blue;
-			pixels[m - 2] = clear;
-	
-			pixels[NUMLEDS - m] = red;
-			pixels[NUMLEDS - m + 2] = clear;
-		}
-		display();
-	}
+    if  ( m >= middle ) {
+      pixels[m] = blue;
+      pixels[m - 2] = clear;
+  
+      pixels[NUMLEDS - m] = red;
+      pixels[NUMLEDS - m + 2] = clear;
+    }
+    display();
+  }
 }
 
 // red white and blue chasers
-void goJoe(unsigned long time)
-{
-	int m;
+void goJoe(unsigned long time) {
+  int m;
 
-	colorwipe(clear); // clear display from existing patterns
-		
-	for ( m = 0; m < NUMLEDS; m++ )
-	{
-		pixels[m] = blue;
-		pixels[m - 2] = red;
-		pixels[m - 4] = white;
-		pixels[m - 6] = clear;
-		display();
-		delayMillis(time);
-	}
-	for ( m = NUMLEDS; m >= 0; m-- )
-	{
-		pixels[m] = clear;
-		pixels[m - 2] = white;
-		pixels[m - 4] = red;
-		pixels[m - 6] = blue;
-		display();
-		delayMillis(time);
-	}
+  colorwipe(clear); // clear display from existing patterns
+    
+  for ( m = 0; m < NUMLEDS; m++ ) {
+    pixels[m] = blue;
+    pixels[m - 2] = red;
+    pixels[m - 4] = white;
+    pixels[m - 6] = clear;
+    display();
+    delayMillis(time);
+  }
+  for ( m = NUMLEDS; m >= 0; m-- ) {
+    pixels[m] = clear;
+    pixels[m - 2] = white;
+    pixels[m - 4] = red;
+    pixels[m - 6] = blue;
+    display();
+    delayMillis(time);
+  }
 }
 
 // send random colors down each pixel
-void randomdance(void)
-{
-	int m;
-		
-		for ( m = 0; m < NUMLEDS; m++ )
-		{
-			pixels[m] = adcGenRand24();
-			display();
-		}
+void randomdance(void) {
+  int m;
+    
+    for ( m = 0; m < NUMLEDS; m++ ) {
+      pixels[m] = adcGenRand24();
+      display();
+    }
 }
 
-void solidblink(unsigned long c)
-{
-	colorwipe(c);
-	delayMillis(500);
-	colorwipe(clear);
+void solidblink(unsigned long c) {
+  colorwipe(c);
+  delayMillis(500);
+  colorwipe(clear);
 }
 
 // animate fading rainbow cycle
-void rainbowcycle(void)
-{
-	int k, j;
-	
-	for ( j=0; j<256; j++ )
-	{
-		for ( k=0; k < NUMLEDS; k++ )
-		{
-			setPixelS(k, wheel( ( k+j) % 255 ) );
-		}
-		display();
-		delayMillis(100);
-	}
+void rainbowcycle(void) {
+  int k, j;
+  
+  for ( j=0; j<256; j++ ) {
+    for ( k=0; k < NUMLEDS; k++ ) {
+      setPixelS(k, wheel( ( k+j) % 255 ) );
+    }
+    display();
+    delayMillis(100);
+  }
 }
 
 // display static rainbow
-void showrainbow(void)
-{
-	int k, j;
-	
-	for ( j=0; j < 256 * 5; j++ )
-	{
-		for ( k=0; k < NUMLEDS; k++ )
-		{
-			setPixelS(k, wheel( ((k * 256 / NUMLEDS ) + j) % 255) );
-		}
-	}
-	display();
-	delayMillis(100);
+void showrainbow(void) {
+  int k, j;
+  
+  for ( j=0; j < 256 * 5; j++ ) {
+    for ( k=0; k < NUMLEDS; k++ ) {
+      setPixelS(k, wheel( ((k * 256 / NUMLEDS ) + j) % 255) );
+    }
+  }
+  display();
+  delayMillis(100);
 }
-			
+      
 // wipe strip to selected color
-void colorwipe(unsigned long c)
-{
-	int v;
-	
-	for ( v=0; v < NUMLEDS; v++)
-		setPixelS(v, c);
-	display();
-		//delayMillis(100);
+void colorwipe(unsigned long c) {
+  int v;
+  
+  for ( v=0; v < NUMLEDS; v++)
+    setPixelS(v, c);
+  display();
+    //delayMillis(100);
 }
 
 // run all functions as demo
-void demos(void)
-{
-	int x;
-		
+void demos(void) {
+  int x;
+    
 // run demos for display
-		
-	for (x = 0; x < 3; x++)
-	{
-		copcar();
-	}
-		
-	for (x = 0; x < 3; x++)
-	{
-		goJoe(50);
-	}
+    
+  for (x = 0; x < 3; x++) {
+    copcar();
+  }
+    
+  for (x = 0; x < 3; x++) {
+    goJoe(50);
+  }
 
-	for (x = 0; x < 5; x++)
-	{
-		randomdance();
-	}
+  for (x = 0; x < 5; x++) {
+    randomdance();
+  }
 
-	colorwipe(clear);
-		
-	for (x = 0; x < 3; x++)
-	{
-		solidblink(green);
-	}
+  colorwipe(clear);
+    
+  for (x = 0; x < 3; x++) {
+    solidblink(green);
+  }
 
-	for (x = 0; x < 2; x++)
-	{
-		rainbowcycle();
-	}
+  for (x = 0; x < 2; x++) {
+    rainbowcycle();
+  }
 }
 
 /* library functions */
 
 // write a bunch of zeroes, used for latch
 void writezeros(unsigned int n) {
-	unsigned int i;
-	P1OUT &= ~DATA; // Data low
-	for(i = 8 * n; i>0; i--) {
-		P1OUT |= CLOCK;
-		P1OUT &= ~CLOCK;
-	}
+  unsigned int i;
+  P1OUT &= ~DATA; // Data low
+  for(i = 8 * n; i>0; i--) {
+    P1OUT |= CLOCK;
+    P1OUT &= ~CLOCK;
+  }
 }
 
 //initialization
-void init(void)
-{
-	int i;
-	P1DIR |= DATA + CLOCK; // set data and clock pins to output
-	P1OUT &= ~DATA; // Data low
-	P1OUT &= ~CLOCK;
-	for(i=0; i<NUMLEDS; i++) {
-		pixels[i] = 0x808080;
-	}
-	writezeros(3 * ((NUMLEDS + 63) / 64)); // latch to wake it up
+void init(void) {
+  int i;
+  P1DIR |= DATA + CLOCK; // set data and clock pins to output
+  P1OUT &= ~DATA; // Data low
+  P1OUT &= ~CLOCK;
+  for(i=0; i<NUMLEDS; i++) {
+    pixels[i] = 0x808080;
+  }
+  writezeros(3 * ((NUMLEDS + 63) / 64)); // latch to wake it up
 }
 
 // send data to led strip; create patten with a 'use' function then send with display
-void display(void)
-{
-	unsigned long data;
+void display(void) {
+  unsigned long data;
     
     // send all the pixels
-    for ( p=0; p < NUMLEDS ; p++ )
-    {
-        data = pixels[p];
-        // 24 bits of data per pixel
-        for ( i=0x800000; i>0 ; i>>=1 )
-        {
-            if (data & i)
-            {
-                P1OUT |= DATA;
-            }
-            else
-            {
-                P1OUT &= ~DATA;
-            }
-            P1OUT |= CLOCK;    // latch on clock rise
-            P1OUT &= ~CLOCK;
+    for ( p=0; p < NUMLEDS ; p++ ) {
+      data = pixels[p];
+      // 24 bits of data per pixel
+      for ( i=0x800000; i>0 ; i>>=1 ) {
+        if (data & i) {
+            P1OUT |= DATA;
+        } else {
+            P1OUT &= ~DATA;
         }
+        P1OUT |= CLOCK;    // latch on clock rise
+        P1OUT &= ~CLOCK;
+      }
     }
     writezeros(3 * ((NUMLEDS + 63) / 64)); // latch
     delayMillis(3);
 }
 
 // create 24bit color value
-unsigned long color(unsigned char r, unsigned char g, unsigned char b)
-{
-	unsigned long c;
-	
-	c = 0x808080 | ((unsigned long)g << 16) | ((unsigned long)r << 8) | (unsigned long)b;
-	return c;
+unsigned long color(unsigned char r, unsigned char g, unsigned char b) {
+  unsigned long c;
+  
+  c = 0x808080 | ((unsigned long)g << 16) | ((unsigned long)r << 8) | (unsigned long)b;
+  return c;
 }
 // create color value from an unsigned long, so you can use colorHex(0xABCDEF);
-unsigned long colorHex(unsigned long hex)
-{
-	return 0x808080 | hex;
+unsigned long colorHex(unsigned long hex) {
+  return 0x808080 | hex;
 }
 
 // set pixel to specified color
-void setPixel(unsigned int n, unsigned char r, unsigned char g, unsigned char b)
-{
-	if (n > NUMLEDS) return;
-	
-	pixels[n] = color(r, g, b);
+void setPixel(unsigned int n, unsigned char r, unsigned char g, unsigned char b) {
+  if (n > NUMLEDS) return;
+  
+  pixels[n] = color(r, g, b);
 }
 
 //set pixel to color by function
-void setPixelS(unsigned int n, unsigned long c)
-{
-	if ( n > NUMLEDS ) return;
-	
-	pixels[n] = c;
+void setPixelS(unsigned int n, unsigned long c) {
+  if ( n > NUMLEDS ) return;
+  
+  pixels[n] = c;
 }
 
 // rotate colorwheel for rainbows
-unsigned long wheel(unsigned char wheelpos)
-{
-	if (wheelpos <=85)
-	{
-		return color( wheelpos * 3, 255 - wheelpos * 3, 0 );
-	}
-	else if ( wheelpos < 170 )
-	{
-		return color( 255 - wheelpos * 3, 0, wheelpos * 3 );
-	}
-	else
-	{
-		wheelpos -= 170;
-		return color( 0, wheelpos * 3, 255 - wheelpos * 3 );
-	}
+unsigned long wheel(unsigned char wheelpos) {
+  if (wheelpos <=85) {
+    return color( wheelpos * 3, 255 - wheelpos * 3, 0 );
+  }
+  else if ( wheelpos < 170 ) {
+    return color( 255 - wheelpos * 3, 0, wheelpos * 3 );
+  }  else {
+    wheelpos -= 170;
+    return color( 0, wheelpos * 3, 255 - wheelpos * 3 );
+  }
 }
 
 // generate random 24bit number using ADC10 channel 5; leave P1.4 & P1.5 floating
-unsigned long adcGenRand24(void)
-{
+unsigned long adcGenRand24(void) {
   char bit;
   unsigned long random;
   
-  for(bit = 0; bit < 24; bit++){
+  for(bit = 0; bit < 24; bit++) {
     ADC10CTL1 |= INCH_5;
     ADC10CTL0 |= SREF_1 + ADC10SHT_1 + REFON + ADC10ON;
     ADC10CTL0 |= ENC + ADC10SC;
@@ -397,12 +352,12 @@ unsigned long adcGenRand24(void)
 }
 
 // millisecond delay counter using WDT
-void delayMillis(unsigned long milliseconds){
+void delayMillis(unsigned long milliseconds) {
   unsigned long wakeTime = wdtCounter + (milliseconds * WDT_FREQUENCY / 1000);
   while(wdtCounter < wakeTime);
 }
 
 // wdt isr
-interrupt(WDT_VECTOR) watchdog_timer(void){
+interrupt(WDT_VECTOR) watchdog_timer(void) {
   wdtCounter++;
 }
